545c7b8e0d91485316bdf35fd15df46b0f48a1eb
tannersavenge <tannerwilliambrown@gmail.com>
2024-11-09 20:16:03 -0700

    fix

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,7 +4,18 @@ using System.Collections.Generic;
 public class WhyBeMindful
 {
     private string name;
-    private string description
+    private string description;
     private int duration;
+
+    public WhyBeMindful(string name, string description){
+        this.name;
+        this.description = description;
+    }
+
+    public void SetDuration(int duration){
+        this.duration = duration
+    }
+
+    public void 
 
 }
