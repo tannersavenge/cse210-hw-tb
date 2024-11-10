62e09915f2da9e72aac51b940f76793c33c320a6
tannersavenge <tannerwilliambrown@gmail.com>
2024-11-09 21:10:12 -0700

    im litteraly just praying this code works

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -77,12 +77,80 @@ public void PerformBreathing(){
 
         EndTheActivity();
     }
+
+
+    private void Countdown(int seconds)
+    {
+        int remainingSeconds = seconds;
+        
+        // Initialize a timer with a 1-second interval
+        Timer timer = new Timer(1000);  // 1000 ms = 1 second
+        
+        // Define the event handler for each tick of the timer
+        timer.Elapsed += (sender, e) =>
+        {
+            if (remainingSeconds > 0)
+            {
+                Console.Write(remainingSeconds + " ");
+                remainingSeconds--;
+            }
+            else
+            {
+                timer.Stop();
+                Console.WriteLine();
+            }
+        };
+        timer.Start();
+        while (timer.Enabled) { } 
+    }
+}
+
+public class ReflectionActivity : WhyBeMindful {
+    private List<string> prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something selfless."
+    };
+    private List<string> questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience?",
+        "What did you learn about yourself?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+     private List<string> listedItems;
+
+      public ListingActivity() : base("Listing", "This activity will help you think on the good things in your life by having you list as many things as you canabout a specific topic.") {
+        listedItems = new List<string>();
+    }
+
+    public void PerformListing()
+    {
+
+        StartActivity();
+        int duration = GetDuration();
+        Random random = new Random();
+        string chosenPrompt = prompts[random.Next(prompts.Count)];
+        Console.WriteLine($"Consider the following prompt:\n{chosenPrompt}");
+        Console.WriteLine("You will have a few seconds to begin preparing your thoughts.");
+        Countdown(5);
+
+    Console.WriteLine("Start listing items. Type each item and press Enter:");
+        
+    DateTime startTime = DateTime.Now;
+    while ((DateTime.Now - startTime).TotalSeconds < duration)
 }
 
 
 
-
-}
 
 
 
