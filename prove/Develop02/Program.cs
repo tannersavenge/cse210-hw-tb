1f166beff8456868259ce7111fe2938a9c45672f
tannersavenge <tannerwilliambrown@gmail.com>
2024-12-15 15:49:37 -0700

    finishing up part 3

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,39 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Journaltask{
+    class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+            bool run=true;
+            while(run){
+                Console.WriteLine("\nJournal Program");
+                Console.WriteLine("1. Write a New Entry");
+                Console.WriteLine("2. Display All Entries");
+                Console.WriteLine("3. Save Journal to File");
+                Console.WriteLine("4. Load Journal from File");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option: ");
+
+                string choice = Console.ReadLine();
+                switch (choice){
+                    case "1":
+                        Console.WriteLine("enter your journal entry: ");
+                        string content = Console.ReadLine();
+                        journal.AddEntry(content);
+                        break;
+                    case "2":
+                        journal.displayEntries();
+                        break;
+                    case "3":
+                        Console.WriteLine("Enter the name of the file you want to save a great example of this is ( ex: iLoveJournaling.txt)");
+                        string saveFile = Console.ReadLine();
+                        journal.SaveToFile(saveFile);
+                        break;
+
+                }
+            }
+        }
     }
 }
